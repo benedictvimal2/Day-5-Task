78c5ad6a154b3c75618fc747057ef3e64a3be7e0
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-23 10:00:49 +0530

    added batch

--- Task2/script.js
+++ Task2/script.js
@@ -1,5 +1,6 @@
 var json = [{
-    "id" : "vimal", 
+    "id" : "vimal",
+    "batch" : "fsd60wdt", 
     "task" : "day five task",
     "msg"   : "For the given JSON iterate over all for loops",    
     "mail": "benedictvimal2@gmail.com"
@@ -7,6 +8,7 @@ var json = [{
 {
     "id" : "raj", 
     "task" : "day five task",
+    "batch" : "fsd60wdt", 
     "msg"   : "For the given JSON iterate over all for loops",
     "mail": "benedictvimal2@gmail.com"
 }];
@@ -16,18 +18,18 @@ for(var i = 0; i < json.length; i++) {
 
     console.log(obj.id);
     console.log(obj.task);
+    console.log(obj.batch);
     console.log(obj.msg);
     console.log(obj.mail);
 
 }
 //for Each
-json.forEach(function(obj) { console.log(obj.msg); });
+json.forEach(function(obj) { console.log(obj.task); });
 
 //for In
 for (var key in json) {
 if (json.hasOwnProperty(key)) {
-  console.log(json[key].id);
-  //console.log(json[key].msg);
+  console.log(json[key].batch);
  
 }
 }
