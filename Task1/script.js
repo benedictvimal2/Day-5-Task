52c1867fb8843eb1ccf66455bf7fa4b9ec6264b5
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-23 09:30:49 +0530

    Task 1 done

---
+++ Task1/script.js
@@ -0,0 +1,59 @@
+let myResume={
+    "basics": {
+        "name": "Vimal Raj M",
+        "mail": "benedictvimal2@gmail.com",
+        "mobile": 9578741769,
+        "degree": "B.Tech",
+        "specialization": "Mechanical Engineering",
+        "location": {
+        "address": "No-9 Anna Nagar Kovaipudur",
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+        "country": "India",
+        "pinCode": 641042
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/vimal-raj-226587136/",
+          "github":"https://github.com/benedictvimal2/"
+        }
+      ]
+    },
+    "profession": [
+      {
+        "company": "SmackCram Technologies",
+        "position": "Mechanical Design Engineer",
+        "startDate": "01-04-2019",
+        "endDate": "31-03-2022",
+        "summary": "i am a mechanical design engineer creating and modifying food machines with precision",
+      },
+    ],
+    "education": [
+      {
+        "university": "Karunya Univesity",
+        "department": "Mechnaical",
+        "studyType": "fulltime",
+        "startYear": 2008,
+        "endYear": 2012,
+        "gpa": 6.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "solidworks,autocad",
+        "level": "professional",
+        "project": "automatic peanut bar manufacturing unit"
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "interest": "sports,vehicle customization,",
+      }
+    ]
+  }
+  console.log(myResume);
